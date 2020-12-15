a0bf57f368361030f4fc8fbeb4ccdc3dc62759b2
Ubuntu <ubuntu@ip-172-31-91-51.ec2.internal>
2020-12-15 20:31:26 +0000

    Adding new route and hostname

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,2 +1,42 @@
-package io.pivotal.pal.tracker;public class EnvController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sound.sampled.Port;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+    private final String port;
+    private final String memory_limit;
+    private final String cfInstanceIndex;
+    private final String cfInstanceAddr;
+
+    public EnvController(@Value("${port:NOT SET}") String port,
+                         @Value("${memory.limit:NOT SET}") String memory_limit,
+                         @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+                         @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr){
+        this.port = port;
+        this.memory_limit = memory_limit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddr = cfInstanceAddr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv(){
+        Map<String, String> env = new HashMap<>();
+
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memory_limit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+
+        return env;
+        //return Map.of("PORT", port, "MEMORY_LIMIT", memory_limit,"CF_INSTANCE_INDEX",cfIntanceIndex, "CF_INSTANCE_ADDR", cfInstanceAddr);
+    }
+
+
 }
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,2 +1,19 @@
-package io.pivotal.pal.tracker;public class WelcomeController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class WelcomeController {
+    private final String message;
+
+    public WelcomeController(@Value("${welcome.message}")String message){
+        this.message = message;
+    }
+
+    @GetMapping("/")
+    public String sayHello(){
+        return message;
+    }
 }
